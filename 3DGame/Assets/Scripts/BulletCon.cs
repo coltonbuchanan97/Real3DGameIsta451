0921b114a4f41a4333908829005aaedd36144381
Colton Buchanan <coltonbuchanan@email.arizona.edu>
2018-10-16 12:34:35 -0700

    Can Fire Bullets but still needs work

---
+++ 3DGame/Assets/Scripts/BulletCon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCon : MonoBehaviour {
+    public float lifetime;
+    float currentLife;
+    public float speed;
+
+    public shotGun owner;
+
+    void Update()
+    {
+        currentLife += Time.deltaTime;
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        if (currentLife > lifetime)
+        {
+            Destroy(this.gameObject);
+            owner.ProjectileDestroyed(this);
+        }
+    }
+}
---
+++ 3DGame/Assets/Scripts/shotGun.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shotGun : MonoBehaviour {
+
+    public BulletCon projectile;
+    public float projectileSpawnDistance;
+
+    public int maxProjectiles;
+
+    public List<BulletCon> projectiles = new List<BulletCon>();
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (projectiles.Count > maxProjectiles) return;
+
+            BulletCon newProjectile = Instantiate<BulletCon>(projectile);
+            newProjectile.transform.position = transform.position + transform.right * projectileSpawnDistance;
+            projectiles.Add(newProjectile);
+            newProjectile.owner = this;
+        }
+    }
+
+    public void ProjectileDestroyed(BulletCon dyingProjectile)
+    {
+        projectiles.Remove(dyingProjectile);
+    }
+}
+
+
