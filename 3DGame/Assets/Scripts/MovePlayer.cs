d81c87d7d4039414e90b0365bde7ef043347b2c1
Colton Buchanan <coltonbuchanan@email.arizona.edu>
2018-10-16 10:56:57 -0700

    Better move and Added Pillars

--- 3DGame/Assets/Scripts/MovePlayer.cs
+++ 3DGame/Assets/Scripts/MovePlayer.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour {
 
-    public float speed;
+    private Rigidbody rigidBody;
+    private Vector3 moveDelta;
+    public float speed = 8f;
 
-    // Use this for initialization
-    void Start () {
-      
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.Translate(speed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, speed * Input.GetAxis("Vertical") * Time.deltaTime);
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+        moveDelta = new Vector3(x, 0.0f, z) * speed * Time.deltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+        RotateIfNewDirection(moveDelta);
+        rigidBody.MovePosition(rigidBody.position + moveDelta);
+    }
+
+    private void RotateIfNewDirection(Vector3 input)
+    {
+        if (input.magnitude < 0.01f)
+            return;
+
+        rigidBody.rotation = Quaternion.LookRotation(input);
     }
 }
